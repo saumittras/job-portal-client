b08d9a70b11c95c9446349fad10d500ee900149b
Saumittra Sarker <saumittra.eee@gmail.com>
2025-03-06 19:38:56 +0600

    added my posted job section

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const MyPostedJobs = () => {
+    const [jobs, setJobs]=useState();
+    const {user}=useAuth();
+
+    useEffect(()=>{
+        fetch(`http://localhost:3000/jobs?email=${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>setJobs(data))
+    },[user?.email])
     return (
         <div>
-            <h3>Hello from my posted Jobs routes</h3>
+            <h3 className='text-5xl'>My Posted Jobs: {jobs?.length}</h3>
         </div>
     );
 };
